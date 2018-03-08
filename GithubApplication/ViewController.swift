85b6faf0905a68fb3e9cadf84af6c9216d57018a
Sina Khanjani <sinakhanjani@gmail.com>
2018-03-08 16:37:17 +0330

    Initial Commit

---
+++ GithubApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubApplication
+//
+//  Created by Sinakhanjani on 12/17/1396 AP.
+//  Copyright © 1396 Sinakhanjani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
